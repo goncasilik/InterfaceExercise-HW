4ef8b6c08e5f5da5651469d3a5636cab801a0757
goncasilik <45707360+goncasilik@users.noreply.github.com>
2020-12-18 19:25:37 +0300

    Interface homework completed

---
+++ Scripts/UI/InfoTextController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoTextController : MonoBehaviour
+{
+    private Text infoText;
+    public Text InfoText { get { return (infoText == null) ? infoText = GetComponent<Text>() : infoText; } }
+
+    public void WriteAppleInfoText()
+    {
+        InfoText.text = "E'ye basıp yersen canın dolar";
+    }
+    public void WriteBulletInfoText()
+    {
+        InfoText.text = "E'ye basıp toplarsan canın azalır";
+    }
+    public void WriteDoorInfoText()
+    {
+        InfoText.text = "E'ye basarsan kapı açılır";
+    }
+
+    private void OnEnable()
+    {
+        EventManager.OnApplePickUpEvent.AddListener(WriteAppleInfoText);
+        EventManager.OnBulletPickUpEvent.AddListener(WriteBulletInfoText);
+    }
+    private void OnDisable()
+    {
+        EventManager.OnApplePickUpEvent.RemoveListener(WriteAppleInfoText);
+        EventManager.OnBulletPickUpEvent.RemoveListener(WriteBulletInfoText);
+    }
+}
